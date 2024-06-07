554d0553ad6592833a162c86e58b5c82a90ac4d5
Toni <89582846+Noccis@users.noreply.github.com>
2024-06-07 16:42:38 +0200

    created a basic keyboard. Did not get the stretch to work yet

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,43 @@
+import "../style/keyboard.css"
+
+const KEYS = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+export function Keyboard() {
+    return(
+        <div id="keyboard">
+            {KEYS.map(key => {
+                return (
+                <button id="letter-buttons" key={key}>
+                    {key}
+                </button>)
+            })}
+        </div>
+    )
+}
